4f2cb31a6c4868c95c02b88f413f56e5945d42e4
Késia Silva <kesiasilvab6@gmail.com>
2025-02-26 10:16:24 -0300

    Add files via upload

---
+++ Aula Py/main.py
@@ -0,0 +1,12 @@
+#biotings
+
+course_name = "Python Programming"
+
+print(len(course_name)) # len() retorna o tamanho da string
+print("primeiro caractere: " + course_name [0]) # []: acessa o caractere na posição 0
+print("ultimo caractere: " + course_name [-1]) # [-1]: acessa o ultimo caractere da string da posição -1
+print("intervalo de caracteres: " + course_name[0:3]) # [0:3]: acessa os 3 caracteres na posição de 0 á 3
+print("todos os caracteres: " + course_name[:]) # [:]: acessa todos os caracteres
+
+
+
