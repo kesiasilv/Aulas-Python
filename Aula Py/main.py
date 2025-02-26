c73d67fbbdba0ed6edfcfb4b1bf4abf786dc5d1b
Késia Silva <kesiasilvab6@gmail.com>
2025-02-26 10:24:19 -0300

    Update main.py

--- Aula Py/main.py
+++ Aula Py/main.py
@@ -7,6 +7,7 @@ print("primeiro caractere: " + course_name [0]) # []: acessa o caractere na posi
 print("ultimo caractere: " + course_name [-1]) # [-1]: acessa o ultimo caractere da string da posição -1
 print("intervalo de caracteres: " + course_name[0:3]) # [0:3]: acessa os 3 caracteres na posição de 0 á 3
 print("todos os caracteres: " + course_name[:]) # [:]: acessa todos os caracteres
+print("intervalo: " + course_name[3:]) # inicia no carac. posição 3 e o restante da string inteira:
 
 
 
