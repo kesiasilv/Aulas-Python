8526e48418ad44fe7d5968187451912bc92f3061
Késia Silva <kesiasilvab6@gmail.com>
2025-03-12 11:13:01 -0300

    Create condicional.py

---
+++ Aula Py/condicional.py
@@ -0,0 +1,9 @@
+## estrutura condicional
+meaning = 30 ## variavel
+
+if meaning > 10:
+    print('true!')
+else:
+    print('false')
+
+print("true!") if meaning > 10 else print("false") ## operador ternário
