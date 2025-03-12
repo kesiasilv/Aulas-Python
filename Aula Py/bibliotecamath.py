a7eebe04a33fdad128c4df45c13b87dab31e5a27
Késia Silva <kesiasilvab6@gmail.com>
2025-03-12 09:59:42 -0300

    Create bibliotecamath.py

---
+++ Aula Py/bibliotecamath.py
@@ -0,0 +1,14 @@
+import math # biblioteca
+
+print(round(2.9)) # arredonda o numero
+print(math.pi) # numero pi 3,14...
+print(abs(-2.9)) # numero absoluto (sinal n importa)
+print(pow(2, 3)) # numero elevado
+print(max(2, 3)) # retorna o maior numero
+print(min(2, 3)) # retorna o menor numero
+print(sum([1, 2, 3])) # retorna a soma
+print(chr(65)) # retorna a letra correspondente
+print(ord("A")) # retorna o numero correspondente
+print(divmod(10, 3)) # retorna o cosciente e resto
+print(math.ceil(2.1)) # arredonda pra cima 
+print(math.floor(2.9)) # arredonda pra baixo
