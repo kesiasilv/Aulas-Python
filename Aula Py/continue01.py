eeae30e0e072f76efe1c339c56996cbd5dc73ca7
Késia Silva <kesiasilvab6@gmail.com>
2025-03-12 11:04:56 -0300

    Create continue01.py

---
+++ Aula Py/continue01.py
@@ -0,0 +1,26 @@
+
+## formatando o tipo de dado
+x = input('digite um numero: ')
+print(type(x)) #mostra o tipo
+y = int(x) + 1 #depois de converter o tipo realiza a soma
+print(y)
+
+x = input('digite outro numero: ')
+z = float(x) + 1 #depois de converter o tipo realiza a soma
+print(z)
+
+w = int(input('digite mais um numero: ')) ## aqui ja formata o dado
+print(w)
+
+n = input('digite seu nome: ')
+print(type(n))
+print(n)
+
+
+## Operadores relacionais
+print(2 > 1)
+print(2 >= 1)
+print(2 < 1)
+print(2 <= 1)
+print(2 == 1)
+print(2 != 1)
