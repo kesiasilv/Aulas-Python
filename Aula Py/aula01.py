8e9ffdaf81c45d1f5ce801d31209518d0339cbd5
Késia Silva <kesiasilvab6@gmail.com>
2025-03-12 09:45:59 -0300

    Create aula01.py

---
+++ Aula Py/aula01.py
@@ -0,0 +1,32 @@
+## Operadores  Aritiméticos
+
+a = 10 #atribuição
+b = 3 #atribuição
+
+s = a + b ## soma
+sub = a - b # subtração
+m = a * b  ## multiplicação
+d = a / b  ## divisão
+df = a // b ## divisao que retorna inteiro
+p = a ** b ## potência
+
+print(s) ## soma
+print("\n")
+
+## atribuições
+x = 10
+print(x)
+
+x += 1
+print(x)
+
+x -= 1
+print(x)
+
+x *= 10
+print(x)
+
+x /= 10
+print(x)
+
+
