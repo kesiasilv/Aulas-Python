27cb1b90b85928bf9001b7cecfa50050f82a272d
Késia Silva <kesiasilvab6@gmail.com>
2025-04-14 10:15:27 -0300

    Update menu.py

--- AT1-N1/menu.py
+++ AT1-N1/menu.py
@@ -1,5 +1,4 @@
-#criar menu de opçoes
-
+# menu de opçoes
 def exibirMenu():
     print("== SISTEMA DE LOJA ==\n")
     print("1. Adicionar itens")
@@ -7,23 +6,48 @@ def exibirMenu():
     print("3. Vizualizar itens")
     print("4. Sair")
 
+def adicionarItem(lista):
+    item = input("\nDigite o item a ser adicionado: ")
+    quantidade = int(input("\nDigite a quantidade: "))
+    lista.append(f"{item} (Quantidade: {quantidade})")
+    print(f"\nO item '{item}' foi cadastrado com sucesso!")
+
+def vizualizarItens(lista):
+    if not lista:
+        print("\nA lista de compras está vazia.")
+    else:
+        print("\nLista de Compras:")
+        for item in range(len(lista)):
+            print(f"- {item}: {lista[item]}")
+
+def removerItem(lista):
+    vizualizarItens(lista)
+    if lista:
+        itemParaRemover = int(input("\nDigite o código do item que deseja remover: "))
+        itemRemovido = lista.pop(itemParaRemover)
+        print(f"\nO item '{itemRemovido}' foi removido com sucesso.")
+
 def executarPrograma():
     continuar = True
+    listaCompras = []
 
     while continuar:
+
+        exibirMenu()
+
         escolha = input("\nEscolha uma opção do Menu: ")
 
         if escolha == "1":
-            print("Adicionar item selecionado")
+            adicionarItem(listaCompras)
         elif escolha == "2":
-            print("Qual item deseja excluir")
+            removerItem(listaCompras)
         elif escolha == "3":
-            print("Lista de Itens:")
+            vizualizarItens(listaCompras)
         elif escolha == "4":
-            print("programa finaizado")
+            continuar = False
+            print("programa finaizado.")
         else:
-            print("Opção invalida")
+            print("\nOpção invalida, Por favor tente novamente.")
 
 
-exibir = exibirMenu()
-executar = executarPrograma()
+executarPrograma()
