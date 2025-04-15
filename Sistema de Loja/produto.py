e2f1b395ca36c3d3b1e59f65ab0d63088653e898
Késia Silva <kesiasilvab6@gmail.com>
2025-04-15 20:27:41 -0300

    Update produto.py

--- Sistema de Loja/produto.py
+++ Sistema de Loja/produto.py
@@ -4,9 +4,5 @@ class Produto:
         self.preco = preco
         self.estoque = estoque
 
-    def aplicar_desconto(self, percentual):
-        desconto = self.preco * (percentual / 100)
-        self.preco -= desconto
-
     def __str__(self):
         return f"{self.nome} - R${self.preco:.2f} (Estoque: {self.estoque})"
