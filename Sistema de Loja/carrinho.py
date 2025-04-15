1fd3e19b6833978e0dfe52456b14851ebdf5b098
Késia Silva <kesiasilvab6@gmail.com>
2025-04-15 20:26:06 -0300

    Update carrinho.py

--- Sistema de Loja/carrinho.py
+++ Sistema de Loja/carrinho.py
@@ -1,23 +1,35 @@
 class Carrinho:
     def __init__(self):
-        self.itens = []  # cada item será um dicionário com 'produto' e 'quantidade'
+        self.itens = []  
 
     def adicionar(self, produto, quantidade):
         if produto.estoque >= quantidade:
             self.itens.append({'produto': produto, 'quantidade': quantidade})
-            produto.estoque -= quantidade
-            print(f"{quantidade}x {produto.nome} adicionado ao carrinho.")
         else:
             print(f"Estoque insuficiente para {produto.nome}.")
 
     def remover(self, nome_produto):
-        for item in self.itens:
-            if item['produto'].nome.lower() == nome_produto.lower():
-                item['produto'].estoque += item['quantidade']
-                self.itens.remove(item)
-                print(f"{item['quantidade']}x {item['produto'].nome} removido do carrinho.")
+     for item in self.itens:
+        if item['produto'].nome.lower() == nome_produto.lower():
+            try:
+                quantidade = int(input("Quantos deseja remover? "))
+                if quantidade <= 0:
+                    print("Quantidade inválida.")
+                    return
+                if quantidade >= item['quantidade']:
+                    item['produto'].estoque += item['quantidade']
+                    self.itens.remove(item)
+                    print(f"Todos os itens de {nome_produto} foram removidos do carrinho.")
+                else:
+                    item['quantidade'] -= quantidade
+                    item['produto'].estoque += quantidade
+                    print(f"{quantidade}x {nome_produto} removido(s) do carrinho.")
                 return
-        print("Produto não encontrado no carrinho.")
+            except ValueError:
+                print("Entrada inválida.")
+                return
+     print("Produto não encontrado no carrinho.")
+
 
     def calcular_total(self):
         return sum(item['produto'].preco * item['quantidade'] for item in self.itens)
@@ -34,4 +46,4 @@ class Carrinho:
 
     def esvaziar(self):
         self.itens.clear()
-        print("Carrinho esvaziado.")
+        print("\n Carrinho esvaziado.\n")
